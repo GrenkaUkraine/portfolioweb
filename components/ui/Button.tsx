c81776d96cefa3206e3675a324fe103eef970e91
GrenkaUkraine <grenkaukrainefromukraine@gmail.com>
2026-08-10 12:58:36 +0300

    feat: update Button component polymorphism

--- components/ui/Button.tsx
+++ components/ui/Button.tsx
@@ -2,32 +2,43 @@ import React from "react";
 import { cn } from "@/lib/utils";
 import Text from "./Text";
 
-export interface ButtonProps extends Omit<
-  React.ButtonHTMLAttributes<HTMLButtonElement>,
-  "title"
-> {
+export interface BaseButtonProps {
   title?: React.ReactNode;
   rightSlot?: React.ReactNode;
   className?: string;
+  children?: React.ReactNode;
 }
 
-export default function Button({
+export type ButtonProps<T extends React.ElementType = "button"> =
+  BaseButtonProps &
+    Omit<React.ComponentPropsWithoutRef<T>, keyof BaseButtonProps> & {
+      as?: T;
+    };
+
+export default function Button<T extends React.ElementType = "button">({
+  as,
   title,
   rightSlot,
   className,
+  children,
   ...props
-}: ButtonProps) {
+}: ButtonProps<T>) {
+  const Component = as || "button";
+
   return (
-    <button
+    <Component
       className={cn(
-        "inline-flex items-center justify-between h-13 w-full px-4 md:h-17 md:px-5",
+        "inline-flex items-center justify-between h-13 w-full px-4 md:h-17 md:px-5 cursor-pointer border border-fg-50 bg-bg hover:border-fg transition-colors disabled:opacity-50 disabled:pointer-events-none",
         className
       )}
       {...props}
     >
-      <Text variant="buttons">{title}</Text>
-
-      {rightSlot && <Text variant="system">{rightSlot}</Text>}
-    </button>
+      <Text variant="buttons">{title || children}</Text>
+      {rightSlot && (
+        <Text variant="system" color="muted">
+          {rightSlot}
+        </Text>
+      )}
+    </Component>
   );
 }
