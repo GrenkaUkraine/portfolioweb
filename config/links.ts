adf777a07c17beb37b067017f74662a7e7421941
GrenkaUkraine <grenkaukrainefromukraine@gmail.com>
2026-08-10 13:26:54 +0300

    feat: add links config

---
+++ config/links.ts
@@ -0,0 +1,24 @@
+export const SOCIAL_LINKS = [
+  {
+    title: "Codebase",
+    rightSlot: "[GitHub]",
+    href: "https://github.com/GrenkaUkraine",
+  },
+  {
+    title: "3D Models",
+    rightSlot: "[ArtStation]",
+    href: "https://artstation.com",
+  },
+  {
+    title: "Designs",
+    rightSlot: "[Behance]",
+    href: "https://behance.net",
+  },
+] as const;
+
+export const NAV_LINKS = [
+  { id: "1", label: "Work", href: "#work" },
+  { id: "2", label: "Playground", href: "#playground" },
+  { id: "3", label: "Stack", href: "#stack" },
+  { id: "4", label: "Contact", href: "#contact" },
+] as const;
