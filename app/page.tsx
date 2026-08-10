362ef6c32fba4aeb29dc6bf79299d10055dd2213
GrenkaUkraine <grenkaukrainefromukraine@gmail.com>
2026-08-10 15:36:57 +0300

    feat: add custom scrollbar styles

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@ import HeroSection from "@/components/sections/HeroSection";
 
 export default function Home() {
   return (
-    <main className="w-full md:h-screen md:overflow-y-auto md:snap-y md:snap-mandatory">
+    <main className="w-full md:h-screen md:overflow-y-auto md:snap-y md:snap-mandatory custom-scrollbar scroll-smooth">
       <HeroSection />
     </main>
   );
